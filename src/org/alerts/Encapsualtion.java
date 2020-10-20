5220a823552aaf827ef6b5ee0087a244534cfeed
Jenith1995 <jeni.28347@gmail.com>
2020-10-20 11:45:39 +0530

    to master

---
+++ src/org/alerts/Encapsualtion.java
@@ -0,0 +1,16 @@
+package org.alerts;
+
+public class Encapsualtion {
+	
+	
+	public static void jen() {
+		
+
+		int i =10;
+	}
+	
+	public static void main(String[] args) {
+		jen();
+	}
+
+}
---
+++ src/org/alerts/Encapsulation2.java
@@ -0,0 +1,13 @@
+package org.alerts;
+
+public class Encapsulation2 {
+	
+	public static void main(String[] args) {
+		
+		Encapsulation2 e = new Encapsulation2();
+		
+		
+		
+	}
+
+}
